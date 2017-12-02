cf3163c2132942d0f22d06d2a77408639e216bd2
sean <sean@DESKTOP-PH0M43P>
2017-12-02 14:43:09 +0800

    405. Convert a Number to Hexadecimal

--- leetcode-1/simple-code.cpp
+++ leetcode-1/simple-code.cpp
@@ -1,34 +1,42 @@
 #include <vector>
 #include <iostream>
 #include <set>
+#include <queue>
+#include <string>
 using namespace std;
 
-set<int> value_set;
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
 
-int ite(int n) {
-	int mod = 0;
-	int addition = 0;
-	while (n) {
-		mod = n % 10;
-		n = n / 10;
-		addition += mod*mod;
+
+// 关于负数的原码、反码、补码的求取：
+// 补码存在的意义在于 便于将减法 用加法的方式实现；
+// 在求反码的时候 符号位保持不变；
+// 补码求反变为原码的时候 符号位同样不变；
+string toHex(int num) {
+	string re;
+	char c_a[16] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
+	int count = 8;
+	int flag = 15;
+	int index = 0;
+	while (count--) {
+		index = num&flag;
+		num >>= 4;
+		re += c_a[index];
 	}
-	return addition;
-}
-bool isHappy(int n) {
-	int re = ite(n);
-	while (re != 1) {
-		if (value_set.count(re) == 0)
-			value_set.insert(re);
-		else
-			return false;
-		re = ite(re);
+	reverse(re.begin(), re.end());
+	while (re.find_first_of('0') == 0) {
+		re.erase(re.begin());
 	}
-	return  true;
-
+	return re;
 }
-
 void main() {
-	bool a = isHappy(7);
-	int b = 0;
+	string a = toHex(26);
 }
