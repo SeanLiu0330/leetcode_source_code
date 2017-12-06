3d37350734a38a1e26cfa38634f558a7849475c3
sean <sean@DESKTOP-PH0M43P>
2017-12-06 17:06:21 +0800

    判断一个数 是否是另一个树 的子树： 关于子树的定义： 子树必须从原始树的某个节点开始 直至末尾完全相同；

--- leetcode-1/simple-code.cpp
+++ leetcode-1/simple-code.cpp
@@ -3,6 +3,7 @@
 #include <set>
 #include <queue>
 #include <string>
+#include <map>
 using namespace std;
 
 /**
@@ -20,23 +21,64 @@ using namespace std;
 // 补码存在的意义在于 便于将减法 用加法的方式实现；
 // 在求反码的时候 符号位保持不变；
 // 补码求反变为原码的时候 符号位同样不变；
-string toHex(int num) {
-	string re;
-	char c_a[16] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
-	int count = 8;
-	int flag = 15;
-	int index = 0;
-	while (count--) {
-		index = num&flag;
-		num >>= 4;
-		re += c_a[index];
-	}
-	reverse(re.begin(), re.end());
-	while (re.find_first_of('0') == 0) {
-		re.erase(re.begin());
-	}
-	return re;
-}
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+ TreeNode* root = new TreeNode(3);
+ TreeNode* root2 = new TreeNode(4);
+
+ bool equalornot(TreeNode* root1, TreeNode* root2, bool& flag) {
+	 if (!flag)
+		 return false;
+	 if ((root1 == NULL && root2 == NULL)) {
+		 return true;
+	 }
+	 else if (root1 == NULL || root2 == NULL) {
+		 flag = false;
+		 return false;
+	 }
+	 else if (root1->val != root2->val)
+		 return flag = false;
+
+	 equalornot(root1->left, root2->left, flag);
+	 equalornot(root1->right, root2->right, flag);
+	 return flag;
+
+
+ }
+ bool isSubtree(TreeNode* s, TreeNode* t) {
+	 bool flag = false;
+	 queue<TreeNode*> treeque;
+	 treeque.push(s);
+	 while (!treeque.empty()) {
+		 bool flag1 = 1;
+		 TreeNode* root1 = treeque.front();
+		 treeque.pop();
+		 if (flag = equalornot(root1, t, flag1)) {
+			 break;
+		 }
+		 if (root1->left != NULL)
+			 treeque.push(root1->left);
+		 if (root1->right != NULL)
+			 treeque.push(root1->right);
+	 }
+	 return flag;
+ }
+
+ 
+
 void main() {
-	string a = toHex(26);
+	root->left = new TreeNode(4);
+	root->right = new TreeNode(5);
+	root->left->left = new TreeNode(1);
+	root->left->right = new TreeNode(2);
+	root2->left = new TreeNode(1);
+	root2->right = new TreeNode(2);
+	bool f = isSubtree(root, root2);
+	bool d = 0;
 }
