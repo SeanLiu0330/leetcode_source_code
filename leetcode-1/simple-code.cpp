6ca82bb463465d4dfdecdb1defb6f37439d047b2
sean <sean@DESKTOP-PH0M43P>
2017-12-06 21:31:04 +0800

    70. Climbing Stairs: --涉及到斐波那契数列的第n项如何获取； --数组不能作为函数的返回值，但是对象、结构体是可以的 --所以用结构体将数组包装起来用于传递数据； --结构体的初始化操作；

--- leetcode-1/simple-code.cpp
+++ leetcode-1/simple-code.cpp
@@ -23,62 +23,50 @@ using namespace std;
 // 补码求反变为原码的时候 符号位同样不变；
 
 
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- };
- TreeNode* root = new TreeNode(3);
- TreeNode* root2 = new TreeNode(4);
-
- bool equalornot(TreeNode* root1, TreeNode* root2, bool& flag) {
-	 if (!flag)
-		 return false;
-	 if ((root1 == NULL && root2 == NULL)) {
-		 return true;
-	 }
-	 else if (root1 == NULL || root2 == NULL) {
-		 flag = false;
-		 return false;
-	 }
-	 else if (root1->val != root2->val)
-		 return flag = false;
-
-	 equalornot(root1->left, root2->left, flag);
-	 equalornot(root1->right, root2->right, flag);
-	 return flag;
-
-
- }
- bool isSubtree(TreeNode* s, TreeNode* t) {
-	 bool flag = false;
-	 queue<TreeNode*> treeque;
-	 treeque.push(s);
-	 while (!treeque.empty()) {
-		 bool flag1 = 1;
-		 TreeNode* root1 = treeque.front();
-		 treeque.pop();
-		 if (flag = equalornot(root1, t, flag1)) {
-			 break;
-		 }
-		 if (root1->left != NULL)
-			 treeque.push(root1->left);
-		 if (root1->right != NULL)
-			 treeque.push(root1->right);
-	 }
-	 return flag;
- }
-
  
-
-void main() {
-	root->left = new TreeNode(4);
-	root->right = new TreeNode(5);
-	root->left->left = new TreeNode(1);
-	root->left->right = new TreeNode(2);
-	root2->left = new TreeNode(1);
-	root2->right = new TreeNode(2);
-	bool f = isSubtree(root, root2);
-	bool d = 0;
+struct matrix {
+	int m[2][2];
+	matrix() {
+		m[0][0] = 1;
+		m[1][1] = 1;
+		m[0][1] = 0;
+		m[1][0] = 0;
+	}
+};
+matrix matrixmul(matrix a, matrix b) {
+	matrix temp = matrix();
+	for (int i = 0; i<2; i++) {
+		for (int j = 0; j<2; j++) {
+			temp.m[i][j] = a.m[i][0] * b.m[0][j] + a.m[i][1] * b.m[1][j];
+		}
+	}
+	return temp;
 }
+matrix Q(matrix base, int n) {
+	matrix ans = matrix();
+	while (n) {
+		if (n & 1)
+			ans = matrixmul(base, ans);
+		base = matrixmul(base, base);
+		n >>=1;
+	}
+	return ans;
+}
+int climbStairs(int n) {
+	matrix base = matrix();
+	matrix origin = matrix();
+	base.m[0][1] = 1;
+	base.m[1][0] = 1;
+	base.m[1][1] = 0;
+	origin.m[1][0] = 1;
+	origin.m[1][1] = 0;
+	matrix final_mat = Q(base, n);
+	matrix re = matrixmul(final_mat, origin);
+	return re.m[1][0];
+
+}
+void main() {
+	int t = climbStairs(44);
+	int c = 0;
+}
+
